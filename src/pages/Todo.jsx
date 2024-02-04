7279661f1e6eb538ab0f1d075e0d81ccb66a598f
Aristine-ig <aristine277@gmail.com>
2024-02-04 17:50:36 +0530

    hii

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
+import { TodoWrapper } from '../components/TodoWrapper'
 
 
 
 const Todo = () => {
 
+  
   return (
     <div className="w-full flex mb-4 items-center justify-between">
-      <div class="todocontainer">
-          <div id="newtask">
-              <input type="text" placeholder="Task to be done.." />
-              <button id="push">Add</button>
-          </div>
-          <div id="tasks"></div>
-      </div>
+      <TodoWrapper />
     </div>
   )
 }
 
 export default Todo
+
+{/* <div className="w-full flex mb-4 items-center justify-between">
+      <div class="todocontainer">
+          <div id="newtask">
+              <input type="text" placeholder="Task to be done.." />
+              <button id="push">Add</button>
+              
+          </div>
+          <div id="tasks"></div>
+      </div>
+    </div> */}
